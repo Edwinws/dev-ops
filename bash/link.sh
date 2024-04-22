da70ec47da431be605690ba5018dc2a0aff68e4f
edwinws <william.edwinx@gmail.com>
2024-04-22 14:51:23 +0800

    Updated link.sh
    - cd into the script path
    - do an ls after it's done to show the symlinks

--- bash/link.sh
+++ bash/link.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# cd, following symlink https://stackoverflow.com/a/51651602/1014038
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 # create symlinks of all executable files in the current directory (recursive) to $HOME/.local/bin except current file using find
 find . -type f | while read -r file
 do
@@ -11,3 +14,5 @@ do
         ln -s -f "$(realpath "$file")" "$HOME/.local/bin/$(basename "$file")"
     fi
 done
+
+ls -l "$HOME/.local/bin"
